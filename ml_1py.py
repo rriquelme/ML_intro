31fe81ba0be5a577e6e1c5db8348efe62667a699
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-02-18 13:38:28 -0300

    iris to dataframe

--- ml_1py.py
+++ ml_1py.py
@@ -1,9 +1,16 @@
 from sklearn.datasets import load_iris
-
+import pandas as pd
 iris_dataset = load_iris()
 # Keys
 print(iris_dataset.keys())
 # description
 #print(iris_dataset['DESCR'])
+# to data frame:
 
-print(iris_dataset['target'])
+iris_df = pd.DataFrame(iris_dataset['data'],columns = iris_dataset['feature_names'])
+iris_df['target'] = pd.Series(iris_dataset['target'])
+#print(iris_dataset['target'])
+
+print(iris_df.head())
+
+print(iris_df.describe())
