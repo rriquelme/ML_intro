fdaed37a697088ca0d097fd38e5f9bff4bc4456f
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-02-21 11:34:20 -0300

    k-nearest

--- ml_1py.py
+++ ml_1py.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -26,5 +28,26 @@ print(train.shape)
 #Scatter Plot
 #iris_df.plot(x = iris_dataset['target'][0], y = iris_dataset['target'][1],kind='scatter')
 #pd.plotting.scatter_matrix(iris_df.drop(columns=['target']),hist_kwds={'bins': 20},c = iris_df['target']) # Same as below
-pd.plotting.scatter_matrix(iris_df[iris_dataset['feature_names']],hist_kwds={'bins': 20},c = iris_df['target'])
-plt.show()
+#pd.plotting.scatter_matrix(iris_df[iris_dataset['feature_names']],hist_kwds={'bins': 20},c = iris_df['target'])
+#plt.show()
+
+# k nearest neighbors with a single neighbor
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(train[iris_dataset['feature_names']].values,train['target'].values)
+
+prediction = knn.predict(np.array([[5,2.9,1,0.2]]))
+print(prediction)
+print(iris_dataset['target_names'][prediction])
+
+# Evaluating Model by hand
+
+result_prediction = knn.predict(test[iris_dataset['feature_names']].values)
+print(result_prediction)
+
+print("Final Score: {:.2f}".format(np.mean(test['target']==result_prediction)))
+
+# Evaluating model with built-in function
+
+print(knn.score(test[iris_dataset['feature_names']].values,test['target'].values))
+
+
