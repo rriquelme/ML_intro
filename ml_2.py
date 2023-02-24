25ab7c053b51deb1505571b998f23c95669df73f
Roberto E. Riquelme Sáez <rers.92@gmail.com>
2023-02-24 19:40:04 -0300

    fix mglearn...

--- ml_2.py
+++ ml_2.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
@@ -6,12 +6,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import mglearn
 
+def make_forge():
+    X, y = make_blobs(centers=2, random_state=4, n_samples=30)
+    y[np.array([7, 27])] = 0
+    mask = np.ones(len(X), dtype=np.bool_)
+    mask[np.array([0, 1, 5, 26])] = 0
+    X, y = X[mask], y[mask]
+    return X, y
 
 # generate dataset
-X, y = mglearn.datasets.make_forge()
+X, y = make_forge()
 # plot dataset
 mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
 plt.legend(["Class 0", "Class 1"], loc=4)
 plt.xlabel("First feature")
 plt.ylabel("Second feature")
+plt.show()
 print("X.shape: {}".format(X.shape))
